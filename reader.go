6f7a875205b93fb69a140faf46db0f8d1c8e9419
chai2010 <chaishushan@gmail.com>
2015-07-05 22:07:47 +0800

    imply reader

--- reader.go
+++ reader.go
@@ -7,6 +7,7 @@ package image
 import (
 	"image"
 	"io"
+	"os"
 )
 
 func DecodeConfig(r io.Reader) (cfg image.Config, format string, err error) {
@@ -26,10 +27,38 @@ func DecodeImage(r io.Reader) (m *Image, format string, err error) {
 	return
 }
 
+func LoadConfig(filename string) (cfg image.Config, format string, err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return image.Config{}, "", err
+	}
+	defer f.Close()
+	return DecodeConfig(f)
+}
+
 func Load(filename string) (m image.Image, format string, err error) {
-	panic("TODO")
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, "", err
+	}
+	defer f.Close()
+	return Decode(f)
 }
 
 func LoadImage(filename string) (m *Image, format string, err error) {
-	panic("TODO")
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, "", err
+	}
+	defer f.Close()
+
+	x, format, err := Decode(f)
+	if err != nil {
+		return nil, "", err
+	}
+
+	if m, _ = AsMemPImage(x); m == nil {
+		m = NewImageFrom(x)
+	}
+	return m, format, nil
 }
