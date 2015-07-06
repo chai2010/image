b6942a6b71fa709f64a12c5a77b4188dc197d9f7
chai2010 <chaishushan@gmail.com>
2015-07-06 10:21:04 +0800

    image: update pkgdoc

--- doc.go
+++ doc.go
@@ -12,7 +12,7 @@
 //		DataType     reflect.Kind
 //		Pix          PixSilce
 //
-//		// Stride is the Pix stride (in bytes)
+//		// Stride is the Pix stride (in bytes, must align with SizeofKind(p.DataType))
 //		// between vertically adjacent pixels.
 //		Stride int
 //	}
