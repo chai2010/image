395ca9d28dd636a7e8f6f22e585a24734cef69a9
chai2010 <chaishushan@gmail.com>
2015-07-06 10:00:21 +0800

    image: impl Save

--- writer.go
+++ writer.go
@@ -5,14 +5,42 @@
 package image
 
 import (
+	"fmt"
 	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"io"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func Encode(w io.Writer, m image.Image, format string) error {
-	panic("TODO")
-}
+type Encoder func(w io.Writer, m image.Image) error
 
-func Save(filename string, m image.Image, format string) error {
-	panic("TODO")
+// Save image, if encoder is nil, only support gif/jpeg/png format.
+func Save(filename string, m image.Image, encoder Encoder) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if encoder != nil {
+		return encoder(f, m)
+	}
+
+	ext := strings.ToLower(filepath.Ext(filename))
+	switch {
+	case strings.HasSuffix(ext, ".gif"):
+		return gif.Encode(f, m, nil)
+	case strings.HasSuffix(ext, ".jpeg"):
+		return jpeg.Encode(f, m, nil)
+	case strings.HasSuffix(ext, ".jpg"):
+		return jpeg.Encode(f, m, nil)
+	case strings.HasSuffix(ext, ".png"):
+		return png.Encode(f, m)
+	}
+
+	return fmt.Errorf("image: Save, unknown format: %s", filename)
 }
