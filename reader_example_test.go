b70dd95287506d0698389207d029ceaa7b0ee15f
chai2010 <chaishushan@gmail.com>
2015-07-06 11:11:35 +0800

    image: more example

---
+++ reader_example_test.go
@@ -0,0 +1,43 @@
+// Copyright 2015 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package image_test
+
+import (
+	"fmt"
+	"log"
+
+	ximage "."
+)
+
+func ExampleLoad() {
+	m, format, err := ximage.Load("./testdata/lena.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("format = %s\n", format)
+	fmt.Printf("Rect = %v\n", m.Bounds())
+	// Output:
+	// format = png
+	// Rect = (0,0)-(512,512)
+}
+
+func ExampleLoadImage() {
+	m, format, err := ximage.LoadImage("./testdata/lena.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("format = %v\n", format)
+	fmt.Printf("MemPMagic = %v\n", m.MemPMagic)
+	fmt.Printf("Rect = %v\n", m.Rect)
+	fmt.Printf("Channels = %v\n", m.Channels)
+	fmt.Printf("DataType = %v\n", m.DataType)
+	// Output:
+	// format = png
+	// MemPMagic = MemP
+	// Rect = (0,0)-(512,512)
+	// Channels = 4
+	// DataType = uint8
+}
---
+++ writer_example_test.go
@@ -0,0 +1,62 @@
+// Copyright 2015 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package image_test
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"os"
+
+	ximage "."
+)
+
+func ExampleSave_png() {
+	outfile := "zz_lena.png"
+	defer os.Remove(outfile)
+
+	m, format, err := ximage.Load("./testdata/lena.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ximage.Save(outfile, m, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("format = %s\n", format)
+	// Output:
+	// format = png
+}
+
+func ExampleSave_pngAsJpeg() {
+	outfile := "zz_lena.png"
+	defer os.Remove(outfile)
+
+	m, format, err := ximage.Load("./testdata/lena.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ximage.Save(outfile, m, func(w io.Writer, m image.Image) error {
+		return jpeg.Encode(w, m, nil)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// outfile is a jpeg
+	_, format, err = ximage.Load(outfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("format = %s\n", format)
+	// Output:
+	// format = jpeg
+}
