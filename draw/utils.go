e203577649aad8d6f5354c1580a77733339f1f61
chai2010 <chaishushan@gmail.com>
2015-07-07 13:44:02 +0800

    image/draw: add Pyramid type

---
+++ draw/utils.go
@@ -0,0 +1,19 @@
+// Copyright 2015 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package draw
+
+func maxInt(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func minInt(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
