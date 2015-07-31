71531b5b10ee095af87ce78e75634d5ba78e2ba3
chai2010 <chaishushan@gmail.com>
2015-07-31 15:37:06 +0800

    image/pyramid: add package doc

--- pyramid/pyramid.go
+++ pyramid/pyramid.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package pyramid provides tiled pyramid image support.
 package pyramid
 
 import (
