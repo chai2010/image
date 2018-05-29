25fecf0c08615a36b43526c19489ea1409c9b6b2
chai2010 <chaishushan@gmail.com>
2018-05-29 14:27:28 +0800

    qrcode: add example to show qrcode in terminal

--- qrencoder/hello.go
+++ qrencoder/hello.go
@@ -8,9 +8,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
 	"io/ioutil"
 	"log"
 
+	ximage "github.com/chai2010/image"
 	qr "github.com/chai2010/image/qrencoder"
 )
 
@@ -19,10 +23,49 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = ioutil.WriteFile("zz_qrout.png", c.PNG(), 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print("output: zz_qrout.png\n")
+	sqrcode, err := MakeTerminalQrCodeImage("123", qr.L)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sqrcode)
+}
+
+func MakeTerminalQrCodeImage(text string, level qr.Level) (string, error) {
+	c, err := qr.Encode(text, qr.L)
+	if err != nil {
+		return "", err
+	}
+
+	m := &codeImage{Code: c}
+
+	framebuffer := ximage.NewStringImage(
+		image.Rect(0, 0, c.Size+2, c.Size+2),
+		ximage.MakeTerminalGrayModel(),
+	)
+	draw.Draw(
+		framebuffer, image.Rect(1, 1, c.Size+1, c.Size+1),
+		m, image.Pt(0, 0),
+		draw.Over,
+	)
+
+	s := framebuffer.ToString(ximage.TerminalColor_WHITE)
+	return s, nil
+}
+
+type codeImage struct{ *qr.Code }
+
+func (c *codeImage) Bounds() image.Rectangle { return image.Rect(0, 0, c.Size, c.Size) }
+func (c *codeImage) ColorModel() color.Model { return color.GrayModel }
+
+func (c *codeImage) At(x, y int) color.Color {
+	if c.Black(x, y) {
+		return color.Gray{0x00} // blackColor
+	}
+	return color.Gray{0xFF} // whiteColor
 }
